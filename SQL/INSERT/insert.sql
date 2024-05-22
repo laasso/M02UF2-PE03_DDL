0f843f6395452d571203d35495720faca92086f1
laasso <nrubialesguerrero@gmail.com>
2024-05-22 18:55:34 +0200

    feat: Cargar datos desde archivo CSV a la tabla clients

--- SQL/INSERT/insert.sql
+++ SQL/INSERT/insert.sql
@@ -1,5 +1,6 @@
+USE restaurant;
 
-
+-- Insert Personal
 LOAD DATA LOCAL INFILE '/home/lasso/M02UF2-PE03_DDL/DATA/personal.csv'
 INTO TABLE personal
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
@@ -7,7 +8,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (dni, nom, cognom, email, telefon, adreca, carrec);
 
---Insert Taules 
+-- Insert Taules 
 LOAD DATA LOCAL INFILE '/home/lasso/M02UF2-PE03_DDL/DATA/taules.csv'
 INTO TABLE taules
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
@@ -15,3 +16,10 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (id,capacitat);
 
+-- Insert Clients
+LOAD DATA LOCAL INFILE '/home/lasso/M02UF2-PE03_DDL/DATA/clients.csv'
+INTO TABLE taules
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(dni,nom,telefon,adreca);
