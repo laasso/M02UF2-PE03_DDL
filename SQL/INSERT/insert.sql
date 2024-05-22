e7825a53c8e0ec6ff4a2fdcdf214ed872da81621
laasso <nrubialesguerrero@gmail.com>
2024-05-22 18:50:40 +0200

    insert taules

--- SQL/INSERT/insert.sql
+++ SQL/INSERT/insert.sql
@@ -1,6 +1,15 @@
-LOAD DATA LOCAL INFILE '../DATA/personal.csv'
+LOAD DATA LOCAL INFILE '/home/lasso/M02UF2-PE03_DDL/DATA/personal.csv'
 INTO TABLE personal
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (dni, nom, cognom, email, telefon, adreca, carrec);
+
+--Insert Taules 
+LOAD DATA LOCAL INFILE '/home/lasso/M02UF2-PE03_DDL/DATA/taules.csv'
+INTO TABLE taules
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id,capacitat);
+
